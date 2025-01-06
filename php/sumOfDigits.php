47932f5d44800fb4f713b7df3b57be3fc025789e
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-06 20:10:59 +0530

    just format

--- php/sumOfDigits.php
+++ php/sumOfDigits.php
@@ -1,15 +1,16 @@
 <?php
 
 $number = 12345;
+$n = (string) $number;
 
 $sum = 0;
 $remainder = 0;
 
-for ($i=0; $i < strlen($number); $i++) {
+for ($i = 0; $i < strlen($number); $i++) {
     $remainder = (int) $number % 10;
     $number = $number / 10;
 
     $sum += $remainder;
 }
 
-echo $sum;
+echo "Sum of digits of {$n} is {$sum}.";
